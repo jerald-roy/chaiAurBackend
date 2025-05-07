1ec8e3d67b13a688da9a1723714df305fe040861
jerald-roy <jeraldroyj143@gmail.com>
2025-05-07 13:44:18 +0530

    new user method added

--- setup-professional-backend/src/routes/user.routes.js
+++ setup-professional-backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { loginUser, registerUser , logoutUser , refreshAccessToken , changeCurrentPassword , getCurrentUser ,  updateAccountDetails, updateUserAvatar, updateUserCoverImage} from "../controllers/user.controller.js"
+import { loginUser, registerUser , logoutUser , refreshAccessToken , changeCurrentPassword , getCurrentUser ,  updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory} from "../controllers/user.controller.js"
 import { verifyJWT } from "../middlewares/authCustom.js"
 //here we are using the below import called as upload because we wanna take the files also like(avatar or image)from the user and this upload is nothing but the middleware of the express that we use it to store locally on the server for some time before uploading it to the cloudinary
 import {upload} from "../middlewares/multer.middleware.js"
@@ -27,4 +27,22 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+/*
+post - creates a new resource
+patch - updates part of the existing resources 
+*/
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT, upload.single("cover-image"), updateUserCoverImage)
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile)
+
+router.route("/history").get(verifyJWT,getWatchHistory)
 export default router
